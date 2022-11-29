e52a14cac55821e3e5763617ec9420fce64da919
Oskar <oskar.f.hallberg@gmail.com>
2022-11-29 11:44:19 +0100

    Working

--- Find.java
+++ Find.java
@@ -5,18 +5,27 @@ import java.util.Scanner; // Import the Scanner class to read text files
 public class Find {
   public static void main(String[] args) {
 
+    System.out.println("Please enter your word and file:\n");
     Scanner sc = new Scanner(System.in);
     String arguments = sc.nextLine();
+    String func = arguments.split(" ")[0];
     String searchWord = arguments.split(" ")[1];
     String fileName = arguments.split(" ")[2];
     try {
+      if(!func.toUpperCase().equals("SEARCH")) {
+        System.out.println("No such input function, try again!");
+        return; 
+      }
       File myObj = new File(fileName);
       Scanner myReader = new Scanner(myObj);
+      System.out.println("These are the following lines containing your word:\n");
+      int i = 0;
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
-        if(data.contains(searchWord)) {
-            System.out.println(data);
+        if(data.toUpperCase().contains(searchWord.toUpperCase())) {
+            System.out.println(i + ": " + data + "\n");
         }
+        i++;
       }
       myReader.close();
     } catch (FileNotFoundException e) {
